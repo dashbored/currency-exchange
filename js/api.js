ea14241ea6d1232aa84e380ced30166070121c26
Dashbored <richar_davy@hotmail.com>
2019-03-16 15:40:29 +0100

    Changed API

--- js/api.js
+++ js/api.js
@@ -1,25 +1,19 @@
 var app_id = "e359b7178de94d29bb3fa8082052ca72"
 
 export async function convert(from, to, amount) {
-    var url = "https://openexchangerates.org/api/convert/" + amount +  "/" + from + "/" + to + "?app_id=" + app_id
-
+    //var url = "https://openexchangerates.org/api/convert/" + amount +  "/" + from + "/" + to + "?app_id=" + app_id
+    var url ="https://api.exchangeratesapi.io/latest?base=" + from + "&" + "symbols=" + to;
     var promise = await fetch(url)
     var result = await promise.json()
     return result;
-    // for (const e in result.rates) {
-    //     // console.log("Key: " + e + " Value: " + result.rates[e]);
 
-    // }
 }
 
 export async function getExchangeRates() {
-    var url = "https://openexchangerates.org/api/latest.json?app_id=" + app_id    
-
+    //var url = "https://openexchangerates.org/api/latest.json?app_id=" + app_id    
+    var url = "https://api.exchangeratesapi.io/latest";
     var promise = await fetch(url)
     var result = await promise.json()
     return result;
-    // for (const e in result.rates) {
-    //     // console.log("Key: " + e + " Value: " + result.rates[e]);
 
-    // }
 }
--- js/main.js
+++ js/main.js
@@ -24,19 +24,37 @@ async function setAmount() {
     var to = $("#currencyTwo").val();
     var amount = $("#amount").val();
 
-    var result = await convert(from, to, amount);
+    var result = await convert(from, to);
 
 
     if (radioValue === "buy") {
-
+        $("#result").text("You will have to pay " + Math.round(Object.entries(result.rates)[0][1] * amount*100)/100 + " " + Object.entries(result.rates)[0][0])
     } else if (radioValue === "sell") {
-
+        $("#result").text("You will get " + Math.round(Object.entries(result.rates)[0][1] * amount*100)/100 + " " + Object.entries(result.rates)[0][0])
     }
 }
 
 async function loadDropDown() {
 
-    var results = await getExchangeRates();
+    var timeout = JSON.parse(localStorage.getItem("time"))
+    if (timeout != null) {
+        if (((Date.now() - Date.parse(timeout.toString())) >= (3.6 * 10**6))) {
+            timeout = new Date();
+            localStorage.removeItem("results");
+            localStorage.setItem("time", JSON.stringify(timeout));
+        }
+    } else {
+        timeout = new Date();
+        localStorage.setItem("time", JSON.stringify(timeout));
+    }
+
+    var results = JSON.parse(localStorage.getItem("results"));
+
+    if (results == null) {
+        results = await getExchangeRates();
+        localStorage.setItem("results", JSON.stringify(results));
+    }
+
     for (const rate in results.rates) {
         var opt = document.createElement("option");
         opt.value = rate;
